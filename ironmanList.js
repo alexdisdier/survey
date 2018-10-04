f898985bdd3a82bfc6d9e38fc4d21343141cda57
Alexandre Disdier <alexandredisdier@Alexandres-MacBook-Pro.local>
2018-10-03 21:50:02 -0700

    I changed the purpose of the form. Mostly content

--- ironmanList.js
+++ ironmanList.js
@@ -1,41 +1,37 @@
-{
-"source": [
-  "http:eu.ironman.com/events/triathlon-races.aspx?l=europe#axzz5SoNtp4zR"
-],
+ironmanData([
+  { "city": "Barcelona", "country": "Spain", "date" : "Oct 7th 2018", "difficulty": "easy"},
 
-"employees":[
-    {"firstName":"John", "lastName":"Doe"},
-    {"firstName":"Anna", "lastName":"Smith"},
-    {"firstName":"Peter", "lastName":"Jones"}
-],
+  { "city": "Lanzarote, canary islands", "country": "Spain", "date" : "May 25th 2019", "difficulty": "hard"},
 
-"course":[
-    "flat",
-    "hilly",
-    "scenic",
-    "cityscape",
-    "historic",
-    "exotic"
-],
-
-"ironmans":[
-  { "city": "Barcelona", "country": "Spain", "date" : "Oct 7th 2018"},
   { "city": "Cork", "country": "Ireland", "date" : "Jun 23rd 2019"},
-  { "city": "Frankfurt", "country": "Germany", "date" : "Jun 30th 2019"},
-  { "city": "Haugesund", "country": "Norway", "date" : "Jun 30th 2019"},
-  { "city": "Nice", "country": "France", "date" : "Jun 30th 2019"},
-  { "city": "Klagenfurt", "country": "Austria", "date" : "Jul 7th 2019"},
-  { "city": "Bolton", "country": "England", "date" : "Jul 14th 2019"},
-  { "city": "Vitoria-Gasteiz", "country": "Spain", "date" : "Jul 14th 2019"},
-  { "city": "Zurich", "country": "Switzerland", "date" : "Jul 21st 2019"},
-  { "city": "Hamburg", "country": "Germany", "date" : "Jul 28th 2019"},
-  { "city": "Tallin Harjumaa", "country": "Estonia", "date" : "Aug 3rd 2019"},
-  { "city": "Maastricht-Limburg", "country": "The Netherlands", "date" : "TBD 2019"},
-  { "city": "Kalmar", "country": "Sweden", "date" : "Aug 17th 2019"},
-  { "city": "Copenhagen", "country": "Denmark", "date" : "Aug 18th 2019"},
-  { "city": "Vichy", "country": "France", "date" : "Aug 25th 2019"},
-  { "city": "Pembrokeshire", "country": "Wales", "date" : "Sep 15th 2019"},
-  { "city": "Emilia-Romagna Cervia", "country": "Italy", "date" : "Sep 21st 2019"},
-]
 
-}
+  { "city": "Frankfurt", "country": "Germany", "date" : "Jun 30th 2019"},
+
+  { "city": "Haugesund", "country": "Norway", "date" : "Jun 30th 2019"},
+
+  { "city": "Nice", "country": "France", "date" : "Jun 30th 2019"},
+
+  { "city": "Klagenfurt", "country": "Austria", "date" : "Jul 7th 2019"},
+
+  { "city": "Bolton", "country": "England", "date" : "Jul 14th 2019"},
+
+  { "city": "Vitoria-Gasteiz", "country": "Spain", "date" : "Jul 14th 2019"},
+
+  { "city": "Zurich", "country": "Switzerland", "date" : "Jul 21st 2019"},
+
+  { "city": "Hamburg", "country": "Germany", "date" : "Jul 28th 2019"},
+
+  { "city": "Tallin Harjumaa", "country": "Estonia", "date" : "Aug 3rd 2019"},
+
+  { "city": "Maastricht-Limburg", "country": "The Netherlands", "date" : "TBD 2019"},
+
+  { "city": "Kalmar", "country": "Sweden", "date" : "Aug 17th 2019"},
+
+  { "city": "Copenhagen", "country": "Denmark", "date" : "Aug 18th 2019", "difficulty": "easy"},
+
+  { "city": "Vichy", "country": "France", "date" : "Aug 25th 2019", "difficulty": "easy"},
+
+  { "city": "Pembrokeshire", "country": "Wales", "date" : "Sep 15th 2019"},
+
+  { "city": "Emilia-Romagna Cervia", "country": "Italy", "date" : "Sep 21st 2019"}
+]);
